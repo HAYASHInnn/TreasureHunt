756d9252fe17c6e1ee29db2722b6682b4503d264
HAYASHInnn <fffm572752@gmail.com>
2024-08-24 16:39:02 +0900

    最初のコミット

---
+++ src/main/java/plugin/treasurehunt/TreasureHunt.java
@@ -0,0 +1,17 @@
+package plugin.treasurehunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TreasureHunt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
